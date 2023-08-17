f775f098c400e4eda764c0516002cee917856b83
hwiiy <87405971+hwiiy@users.noreply.github.com>
2023-08-18 00:25:56 +0900

    fix : route 수정, 컴포넌트 렌더링x

--- src/pages/Quiz/Common/QuizMedia.jsx
+++ src/pages/Quiz/Common/QuizMedia.jsx
@@ -1,16 +1,16 @@
-import React, { useState}  from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import "bootstrap/dist/css/bootstrap.min.css";
 import YouTube from "react-youtube";
 import QuizTitle from "../../../component/base/QuizTitle";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 // quiz components
 const Contents = styled.div`
   background-color: light-gray;
   height: 55vh;
   width: 65vw;
-  display:flex;
+  display: flex;
   flex-direction: column;
   margin: 0 20vw;
   font-size: 1.5rem;
@@ -25,7 +25,6 @@ const Btn = styled.div`
 `;
 //버튼위치다시조정
 
-
 const YoutubeVideo = ({ videoId }) => {
   const opts = {
     height: "490",
@@ -39,16 +38,24 @@ const YoutubeVideo = ({ videoId }) => {
   return <YouTube videoId={videoId} opts={opts} />;
 };
 
-
-export default function QuizMedia( { youtube_link, title, totalPageCount } ) {
+export default function QuizMedia({
+  youtube_link,
+  title,
+  totalPageCount,
+  studentNumber = 1,
+  studyNumber = 1,
+}) {
+  console.log("Student Number:", studentNumber);
+  console.log("Study Number:", studyNumber);
   const navigate = useNavigate();
   const [currentPage, setCurrentPage] = useState(1);
+  const nextStudentNumber = parseInt(studentNumber) + 1;
+  const nextStudyNumber = parseInt(studyNumber) + 1;
 
   if (!youtube_link) {
     return null;
   }
   const extractYoutubeVideoId = (url) => {
-    
     const match = url.match(
       /(?:https?:\/\/)?(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?[\w\?=]*)?/
     );
@@ -57,17 +64,27 @@ export default function QuizMedia( { youtube_link, title, totalPageCount } ) {
 
   const videoId = extractYoutubeVideoId(youtube_link);
 
-  const handlePageChange = (page) => {
-    navigate(`/study/${currentPage}`);
-    setCurrentPage(page);
+  const handleRightClick = (nextStudentNumber, nextStudyNumber) => {
+    const nextUrl = `/student/${parseInt(nextStudentNumber)}/study/${parseInt(
+      nextStudyNumber
+    )}`;
+    navigate(nextUrl);
   };
-
   return (
     <>
-      <QuizTitle text={title} currentPage={currentPage} totalPageCount={totalPageCount}/>
+      <QuizTitle
+        text={title}
+        currentPage={currentPage}
+        totalPageCount={totalPageCount}
+      />
       <Contents>
         <p>
-          <img width="40" height="40" src="https://img.icons8.com/ios/50/19a05e/cinema---v1.png" alt="cinema---v1"/>
+          <img
+            width="40"
+            height="40"
+            src="https://img.icons8.com/ios/50/19a05e/cinema---v1.png"
+            alt="cinema---v1"
+          />
           영상을 시청해주세요!
         </p>
         <div className="youtube">
@@ -76,10 +93,14 @@ export default function QuizMedia( { youtube_link, title, totalPageCount } ) {
       </Contents>
 
       <Btn>
-          <img onClick={() => handlePageChange(currentPage + 1)}
-           width="80" height="80" src="https://img.icons8.com/ios/80/19A05E/circled-right-2.png" alt="circled-left-2"/>
+        <img
+          width="80"
+          height="80"
+          src="https://img.icons8.com/ios/80/19A05E/circled-right-2.png"
+          alt="circled-left-2"
+          onClick={() => handleRightClick(nextStudentNumber, nextStudyNumber)}
+        />
       </Btn>
-      
     </>
   );
 }
--- src/pages/Quiz/Student/Question/ChoiceQuestion.jsx
+++ src/pages/Quiz/Student/Question/ChoiceQuestion.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import QuestionPage from "../../../../component/base/QuestionPage";
@@ -10,7 +10,7 @@ import AnswerTeacher from "../../Teacher/AnswerTeacher";
 
 const ChoiceQuestion = () => {
   const [questions, setQuestions] = useState(null);
-
+  const navigate = useNavigate();
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -28,32 +28,81 @@ const ChoiceQuestion = () => {
   if (!questions) {
     return null;
   }
-  console.log(questions.quizEntityList);
+  // console.log(questions.quizEntityList);
 
+  const handleNextClick = (nextStudentNumber, nextStudyNumber) => {
+    const nextUrl = `/student/${parseInt(nextStudentNumber)}/study/${parseInt(
+      nextStudyNumber
+    )}`;
+    navigate(nextUrl);
+  };
   return (
     <>
-    <Routes>
-      <Route path="/" element={<QuizPage />} />
-      <Route path="complete" element={<QuizComplete />} />
-      <Route path="answerquestion" element={<AnswerQuestion title={questions.title} questionText={questions.question}  question_number={questions.quizEntityList.length} totalPageCount={questions.quizEntityList.length+1} />} />
-      <Route path="answerteacher" element={<AnswerTeacher title={questions.title} questionText={questions.question}  question_number={questions.quizEntityList.length} totalPageCount={questions.quizEntityList.length+1}/>} />
-      <Route path="quizmedia" element={<QuizMedia title={questions.title} youtube_link = {questions.youtube_link} totalPageCount={questions.quizEntityList.length+1} />} />
-        {questions.quizEntityList.map((question, index) => (
-          <Route key={index} 
-            path={`:question_number`} 
-            element={ 
-            <QuestionPage
+      <Routes>
+        <Route path="/" element={<QuizPage />} />
+        <Route path="complete" element={<QuizComplete />} />
+        <Route
+          path="answerquestion"
+          element={
+            <AnswerQuestion
               title={questions.title}
-              questionText={question.question}
-              choices={question.exampleList}
-              totalPageCount={questions.quizEntityList.length+1}
-              question_number={question.id}
-              writer={question.writer}
-              comment={question.comment}
-            />}
+              questionText={questions.question}
+              question_number={questions.quizEntityList.length}
+              totalPageCount={questions.quizEntityList.length + 1}
+            />
+          }
+        />
+        <Route
+          path="answerteacher"
+          element={
+            <AnswerTeacher
+              title={questions.title}
+              questionText={questions.question}
+              question_number={questions.quizEntityList.length}
+              totalPageCount={questions.quizEntityList.length + 1}
+            />
+          }
+        />
+        <Route
+          path="quizmedia"
+          element={
+            <QuizMedia
+              title={questions.title}
+              youtube_link={questions.youtube_link}
+              totalPageCount={questions.quizEntityList.length + 1}
+              studentNumber={questions.studentNumber}
+              studyNumber={questions.studyNumber}
+            />
+          }
+        />
+        {questions.quizEntityList.map((question, index) => (
+          <Route
+            key={index}
+            // question_number === studyNumber ?path={`/study/${question.studyNumber}`} : null
+            //question_number를 찾을 수 없음 ㅠㅠ
+            // path={`:question_number`}
+            // path={`/study/${question.studyNumber}`}
+            element={
+              <QuestionPage
+                title={questions.title}
+                questionText={question.question}
+                choices={question.exampleList}
+                totalPageCount={questions.quizEntityList.length + 1}
+                question_number={question.id}
+                writer={question.writer}
+                comment={question.comment}
+                onNextClick={() =>
+                  handleNextClick(
+                    question.nextStudentNumber,
+                    question.nextStudyNumber
+                  )
+                }
+                navigate={navigate}
+              />
+            }
           />
         ))}
-    </Routes>
+      </Routes>
     </>
   );
 };
