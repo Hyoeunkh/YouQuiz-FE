39b2c7a5a1c07a7074b075e8b969fae10ce00a66
Hyoeunkh <lhe4528@knu.ac.kr>
2023-08-17 18:03:25 +0900

    fix: login API url 일단 연결

--- src/lib/api/auth.js
+++ src/lib/api/auth.js
@@ -1,11 +1,11 @@
 import client from './client'
 
 export const login = ({userid, password}) =>
-    client.post('/api/login/student', {userid, password});
+    client.post('http://101.101.219.109/login/student', {userid, password});
 
 export const register = ({userid, password, username, birth, sex, phoneNumber, school, grade, classnum, code}) =>
-    client.post('/api/register/student', {userid, password, username, birth, sex, phoneNumber, school, grade, classnum, code});
+    client.post('http://101.101.219.109/register/student', {userid, password, username, birth, sex, phoneNumber, school, grade, classnum, code});
 
-export const check = () => client.get('/api/auth/check');
+//export const check = () => client.get('http://101.101.219.109/auth/check');
 
-export const logout = () => client.post('/api/auth/logout');
+//export const logout = () => client.post('http://101.101.219.109/auth/logout');
